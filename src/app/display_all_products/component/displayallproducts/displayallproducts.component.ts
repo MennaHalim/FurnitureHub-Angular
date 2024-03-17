b95bccc079f75f903f4cc2cc887c16425a6f7bbf
mohamed <mohamedahmedagha23@gmail.com>
2024-03-17 09:48:21 +0200

    for card

--- src/app/display_all_products/component/displayallproducts/displayallproducts.component.ts
+++ src/app/display_all_products/component/displayallproducts/displayallproducts.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-displayallproducts',
   standalone: true,
-  imports: [],
+  imports: [MatPaginatorModule],
+  // schemas: [MatPaginator],
+  
   templateUrl: './displayallproducts.component.html',
   styleUrl: './displayallproducts.component.css'
 })
@@ -17,4 +21,22 @@ export class DisplayallproductsComponent {
 }
   //End for checked 
 
+   //for pagination
+  totalItems = 100; // Example total items
+  itemsPerPage = 10;
+  pageSizeOptions = [5, 10, 25, 100];
+  currentPage = 1;
+
+  
+  cards = [
+    { name: 'Product 1', price: 20, oldPrice: 30, imageUrl: '...', sale: true },
+    
+  ];
+
+  get pagedCards() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    return this.cards.slice(startIndex, startIndex + this.itemsPerPage);
+  }
+
+  //end paginaion
 }
