028322c77386104265bf89fdb52f3d3eab137d95
fahd <fahdeldowy75@gmail.com>
2024-03-20 18:47:55 +0200

    add home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,15 @@ import { ShoppingOptionsComponent } from './Products/Components/shopping-options
 import { DisplayallproductsComponent } from "./Products/Components/displayallproducts/displayallproducts.component";
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { BodyComponent } from './body-home/body/body.component';
+import { HeaderComponent } from './Shared/Components/Header/header/header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ShoppingOptionsComponent, DisplayallproductsComponent]
+    imports: [RouterOutlet, ShoppingOptionsComponent, DisplayallproductsComponent,BodyComponent,HeaderComponent]
 })
 export class AppComponent {
   title = 'hub_furniture';
