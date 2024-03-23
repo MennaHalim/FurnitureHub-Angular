05d9882790274a7906430937e866c0b785407586
Fahd Mohammed <150238704+FahdEldowy@users.noreply.github.com>
2024-03-23 15:07:47 +0200

    Add Home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ShoppingOptionsComponent } from './Products/Components/shopping-options/shopping-options.component';
-import { DisplayallproductsComponent } from "./Products/Components/displayallproducts/displayallproducts.component";
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
-// import { BodyComponent } from './body-home/body/body.component';
-import { HeaderComponent } from './Shared/Components/Header/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, ShoppingOptionsComponent, DisplayallproductsComponent,HeaderComponent]
+    imports: [HeaderComponent,BodyComponent,FooterComponent]
 
     // imports: [RouterOutlet, ShoppingOptionsComponent, DisplayallproductsComponent,BodyComponent,HeaderComponent]
 })
