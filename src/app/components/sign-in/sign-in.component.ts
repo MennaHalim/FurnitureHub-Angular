0efff61d1f8c4f3b112c8fd8f0c1bce4e17162db
Fahd Mohammed <150238704+FahdEldowy@users.noreply.github.com>
2024-03-24 03:15:30 +0200

    Sign-In

---
+++ src/app/components/sign-in/sign-in.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign-in',
+  templateUrl: './sign-in.component.html',
+  styleUrls: ['./sign-in.component.css']
+})
+export class SignInComponent implements OnInit {
+
+  signInForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.signInForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
+  }
+
+  onSubmit() {
+   
+    console.log(this.signInForm.value);
+  }
+
+}
