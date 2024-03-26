aa5c301c9cb2aea13b03d792ddd3fdb301c162d1
Mostafa_Ahmed <mostafa.ahmed.mostafa726@gmail.com>
2024-03-26 23:48:51 +0200

    refactoring routes

--- src/app/Layouts/blank-layout/blank-layout.component.ts
+++ src/app/Layouts/blank-layout/blank-layout.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { ShoppingOptionsComponent } from "../../Components/shopping-options/shopping-options.component";
 import { ShopByComponent } from "../../Components/shop-by/shop-by.component";
 import { ProductsComponent } from "../../Components/products/products.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-blank-layout',
     standalone: true,
     templateUrl: './blank-layout.component.html',
     styleUrl: './blank-layout.component.css',
-    imports: [ShoppingOptionsComponent, ShopByComponent, ProductsComponent]
+    imports: [ShoppingOptionsComponent, ShopByComponent, ProductsComponent, RouterOutlet]
 })
 export class BlankLayoutComponent {
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,13 +6,14 @@ import { QueryParamGuard } from './Shared/Guards/query-param.guard';
 import { DetailsComponent } from './Components/details/details.component';
 import { BasketComponent } from './Components/basket/basket.component';
 import { ProductsComponent } from './Components/products/products.component';
+import { ShopByComponent } from './Components/shop-by/shop-by.component';
 
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: BodyComponent, title: 'Home' },
-    { path: 'products/categories', component: BlankLayoutComponent, title: 'Home', canActivate: [QueryParamGuard] },
+    { path: 'products/categories', component: BlankLayoutComponent, title: 'Shop', canActivate: [QueryParamGuard] },
     { path: 'products/categories/:type', component: BlankLayoutComponent, pathMatch: 'full' },
     { path: 'sets/details/:id', component: DetailsComponent, title: 'Details' },
     { path: 'basket', component: BasketComponent, title: 'Cart' },
