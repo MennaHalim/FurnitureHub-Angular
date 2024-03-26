1e4080eacd23d5111ddabd33f90702472da96d46
Mostafa_Ahmed <mostafa.ahmed.mostafa726@gmail.com>
2024-03-26 15:33:30 +0200

    details component workaround

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -3,19 +3,28 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ISet } from '../../Shared/Models/product';
 import { CapitalizeSpacePipe } from "../../Shared/Pipes/capitalize-space.pipe";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-details',
   standalone: true,
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
-  imports: [CapitalizeSpacePipe]
+  imports: [CapitalizeSpacePipe, CommonModule]
 })
 export class DetailsComponent implements OnInit {
 
+  selectedTab: string = 'size-guide'; // Initial selected tab
+
+  selectTab(tab: string): void {
+    this.selectedTab = tab;
+  }
+
+
   setDetails!: ISet
 
-  constructor(private _ActivatedRoute: ActivatedRoute, private _ProductService: ProductService) { }
+  constructor(private _ActivatedRoute: ActivatedRoute,
+    private _ProductService: ProductService) { }
 
   productId!: string | null;
 
@@ -34,16 +43,30 @@ export class DetailsComponent implements OnInit {
     });
   }
 
+  counter: number = 1;
 
 
+  incrementCounter() {
+    this.counter++;
+    this.updateCounterDisplay();
+  }
 
+  decrementCounter() {
+    if (this.counter - 1 > 0) {
+      this.counter--;
+      this.updateCounterDisplay();
+    }
+  }
 
+  updateCounterDisplay() {
+    const numElement = document.querySelector('.num');
+    if (numElement) {
+      numElement.textContent = this.counter.toString().padStart(2, '0');
+    }
+  }
 
-
-
-
-
-
-
+  isAvailable(): boolean {
+    return this.setDetails?.availability === 'InStock';
+  }
 
 }
