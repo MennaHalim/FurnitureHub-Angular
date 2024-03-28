10db7b757fb26bbfa1d7f75218b9a0d39603ae9b
Fahd Mohammed <150238704+FahdEldowy@users.noreply.github.com>
2024-03-28 16:37:39 +0200

    search bar

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -13,13 +13,13 @@ export class SearchComponent {
     onSearch(event: Event) {
       event.preventDefault();
     
-      // Type assertion: cast event.target to HTMLFormElement
+      
       const form = event.target as HTMLFormElement;
     
       // Access form element properties safely
       const queryInput = form.elements.namedItem('query') as HTMLInputElement; // Explicitly specify type as HTMLInputElement
       const query = queryInput.value;
     
-      // ... rest of your logic
+     
     }
   }
